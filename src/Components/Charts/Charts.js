7fc6eef21ebfe31f147c0961e1f9e9308ec936a6
Arezoo Moradi <moradi.arezoo1992@gmail.com>
2021-12-29 11:27:42 +0330

    add drawCanvas function in projects

--- src/Components/Charts/Charts.js
+++ src/Components/Charts/Charts.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { BarChart } from "../../tools/bar";
 import styles from "./Charts.module.css";
 
@@ -6,39 +6,39 @@ const Charts = () => {
     const chart = useRef(null);
 
     useEffect(() => {
-        
         const min = 1;
         const max = 200;
         let data = [
-            {label: 'Jan', value: 32},
-            {label: 'Feb', value: 177},
-            {label: 'March', value: 123},
-            {label: 'April', value: 55},
-            {label: 'May', value: 67},
-        ]
-    
-    
+            { label: "Jan", value: 32 },
+            { label: "Feb", value: 177 },
+            { label: "March", value: 123 },
+            { label: "April", value: 55 },
+            { label: "May", value: 67 },
+        ];
+
         // Charts Specifications
-        const targetId = chart;
+        const targetId = chart.current;
         const canvasWidth = 600;
-        const canvasHeight = 450
-    
-    
-        function getRandomInt(min , max) {
+        const canvasHeight = 450;
+
+        function getRandomInt(min, max) {
             min = Math.ceil(min);
             max = Math.floor(max);
-            return Math.floor(Math.random() * (max - min) + min)
+            return Math.floor(Math.random() * (max - min) + min);
         }
         // Create Chart
-      
-            const new_chart = new BarChart(targetId, canvasWidth, canvasHeight , data)
-      
 
-    }, [])
+        const new_chart = new BarChart(
+            "chart",
+            canvasWidth,
+            canvasHeight,
+            data
+        );
+    }, []);
 
     return (
         <>
-            <div className={styles.Charts} ref={chart} id='chart'>
+            <div className={styles.Charts} ref={chart} id="chart">
                 this will be our bar chart
             </div>
         </>
--- src/tools/bar.js
+++ src/tools/bar.js
@@ -1,80 +1,78 @@
 /**
- * 
+ *
  * bar.js
  * simple, elegant bar chart library
  * {data} -version 1.0
- * {url} 
- * 
- * 
+ * {url}
+ *
+ *
  * Copyright 2021 by Arezoo Moradi
  * {Licence url}
- *  
+ *
  */
 
-'use strickt'
-export function BarChart( targetId, width , height, data) {
-
+"use strickt";
+export function BarChart(targetId, width, height, data) {
     let chart = this;
 
-
     // Specify Configurations
-    chart.configureChart(targetId, width , height, data)
-
+    chart.configureChart(targetId, width, height, data);
 
     // pre Opeations
     chart.performPreOperations();
-    console.log("arezoo : ",chart)
+
+    // Draw Chart
+    chart.drawChart();
+    console.log("arezoo : ", chart);
 }
 
-
-BarChart.prototype.configureChart = function ( targetId, width , height, data) {
+BarChart.prototype.configureChart = function (targetId, width, height, data) {
     let chart = this;
 
-   // Global Canvas Specifications
-   chart.setCanvasParameters(targetId, width, height, data)
+    // Global Canvas Specifications
+    chart.setCanvasParameters(targetId, width, height, data);
 
+    // Global Chart Specifications
+    chart.setChartParameters(targetId, width, height, data);
+};
 
-   // Global Chart Specifications 
-   chart.setChartParameters(targetId, width, height, data)
+BarChart.prototype.setCanvasParameters = function (
+    targetId,
+    width,
+    height,
+    data
+) {
+    let chart = this;
 
- 
-}
-
-BarChart.prototype.setCanvasParameters = function (targetId, width, height, data) {
-   let chart = this;
-   
     chart.id = targetId;
     chart.width = width;
     chart.height = height;
     chart.data = data;
-}
-
+};
 
 BarChart.prototype.setChartParameters = function () {
-
     let chart = this;
-       // chart specifications
+    // chart specifications
 
-       chart.axeRatio = 10;
-       chart.verticalMargin = (chart.height * chart.axeRatio) / 100;
-       chart.horizontalMargin = (chart.width * chart.axeRatio) / 100;
-       chart.axeColor = '#b1b1b1';
-       chart.axeWidth = 0.75;
-   
-   
-       // Label Configurations
-       chart.fontRatio = 3 ;
-       chart.fontFamily = 'times';
-       chart.fontWeight = '300';
-       chart.fontColor = '#666';
-       chart.verticalFontSixe  = (chart.height * chart.fontRatio ) / 100
-       chart.horizontalFontSixe  = (chart.width * chart.fontRatio ) / 100
-   
-   
-       // GuideLine Configurations 
-       chart.guideLineColor = '#e5e5e5';
-       chart.guideLineWidth = 0.5;
-}
+    //Axis Configurations
+    chart.axisRatio = 10;
+    chart.verticalMargin = (chart.height * chart.axisRatio) / 100;
+    chart.horizontalMargin = (chart.width * chart.axisRatio) / 100;
+    chart.axisColor = "#b1b1b1";
+    chart.axisWidth = 0.75;
+
+    // Label Configurations
+    chart.fontRatio = 3;
+    chart.fontFamily = "times";
+    chart.fontWeight = "300";
+    chart.fontColor = "#666";
+    chart.verticalFontSixe = (chart.height * chart.fontRatio) / 100;
+    chart.horizontalFontSixe = (chart.width * chart.fontRatio) / 100;
+
+    // GuideLine Configurations
+    chart.guideLineColor = "#e5e5e5";
+    chart.guideLineWidth = 0.5;
+};
 
 BarChart.prototype.performPreOperations = function () {
     let chart = this;
@@ -83,32 +81,26 @@ BarChart.prototype.performPreOperations = function () {
     chart.createCanvas();
 
     // Get Data
-    chart.handleData()
-
-}
-
+    chart.handleData();
+};
 
 BarChart.prototype.createCanvas = function () {
     let chart = this;
-    // Create Canvas 
-    let canvas = document.createElement('canvas')
-    canvas.id = chart.id + '-' + Math.random();
+    // Create Canvas
+    let canvas = document.createElement("canvas");
+    canvas.id = chart.id + "-" + Math.random();
     canvas.width = chart.width;
     canvas.height = chart.height;
 
-
     // Append canvas to target container
-    document.getElementById(chart.id).innerHTML = ''; //clean container
-    document.getElementById(chart.id).appendChild(canvas)
-
-
+    document.getElementById(chart.id).innerHTML = ""; //clean container
+    document.getElementById(chart.id).appendChild(canvas);
 
     // Add Canvas to chart object
 
     chart.canvas = canvas;
-    chart.context = canvas.getContext('2d')
-
-}
+    chart.context = canvas.getContext("2d");
+};
 
 BarChart.prototype.handleData = function () {
     let chart = this;
@@ -116,9 +108,49 @@ BarChart.prototype.handleData = function () {
     chart.labels = [];
     chart.values = [];
 
-
     chart.data.forEach((item) => {
-        chart.labels.push(item.labels)
-        chart.values.push(item.value)
-    })
+        chart.labels.push(item.label);
+        chart.values.push(item.value);
+    });
+};
+
+BarChart.prototype.preapareData = function () {
+    // Base
+    var chart = this;
+
+    // Global Variables
+    chart.itemsNum = chart.data.length;
+    chart.maxValue = Math.max.apply(null, chart.values);
+    chart.minValue = Math.min.apply(null, chart.values);
+
+    // Axis Specifications
+    chart.verticalAxisWidth = chart.height - 2 * chart.verticalMargin; // bottom and top margins
+    chart.horizontalAxisWidth = chart.width - 2 * chart.horizontalMargin; // left and right margins
+
+    // Label Specifications
+    chart.verticalUpperBound = Math.ceil(chart.maxValue / 10) * 10;
+    chart.verticalLabelFreq = chart.verticalUpperBound / chart.itemsNum;
+    chart.horizontalLabelFreq = chart.horizontalAxisWidth / chart.itemsNum;
+};
+
+BarChart.prototype.drawChart = function () {
+    // Base
+    let chart = this;
+
+    // Vertical Axis
+    chart.drawVerticalAxis();
+};
+
+
+BarChart.prototype.drawVerticalAxis = function () {
+     // Base
+     let chart = this;
+
+    //  Vertical Axis 
+    chart.context.beginPath();
+    chart.context.strokeStyle = chart.axisColor;
+    chart.context.lineWidth = chart.axisWidth;
+    chart.context.moveTo(chart.horizontalMargin, chart.verticalMargin)
+    chart.context.lineTo(chart.horizontalMargin, chart.height - chart.verticalMargin)
+    chart.context.stroke()
 }
